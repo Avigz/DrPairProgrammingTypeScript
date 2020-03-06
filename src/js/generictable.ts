72bae27ef5f331153ee942cf21b3e33aae476c7e
Anders Børjesson <anbo@zealand.dk>
2020-03-06 09:18:38 +0100

    refactored to forEach

--- src/js/generictable.ts
+++ src/js/generictable.ts
@@ -4,15 +4,14 @@ export function json2table100(json: any): string {
     let cols: string[] = Object.keys(json[0]);
     let headerRow: string = "";
     let bodyRows: string = "";
-    cols.map(function (col: string): void {
-        headerRow += "<th>" + capitalizeFirstLetter(col) + "</th>";
+    cols.forEach((colName: string) => {
+        headerRow += "<th>" + capitalizeFirstLetter(colName) + "</th>"
     });
-    json.map(function (row: any): void {
+    json.forEach((row: any) => {
         bodyRows += "<tr>";
         // loop over object properties and create cells
-        cols.map(function (colName: string): void {
+        cols.forEach((colName: string) => {
             bodyRows += "<td>" + (typeof row[colName] === "object" ? JSON.stringify(row[colName]) : row[colName]) + "</td>";
-            // error in article slash missing (/td)
         });
         bodyRows += "</tr>";
     });
